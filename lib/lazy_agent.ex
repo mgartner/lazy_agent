9f5b0a1fb281c1a05ce035f2382b3372d3b2144f
Marcus Gartner <marcus@lob.com>
2018-04-28 12:09:42 -0700

    support for start_link/2 and get/2

--- lib/lazy_agent.ex
+++ lib/lazy_agent.ex
@@ -1,18 +1,68 @@
 defmodule LazyAgent do
   @moduledoc """
+  TODO
   Documentation for LazyAgent.
-  """
-
-  @doc """
-  Hello world.
 
   ## Examples
 
-      iex> LazyAgent.hello
-      :world
+      iex> {:ok, pid} = LazyAgent.start_link(fn -> 42 end)
+      iex> LazyAgent.get(pid, fn state -> state end)
+      42
+
+      iex> LazyAgent.start_link(fn -> 42 end, name: :lazy)
+      iex> LazyAgent.get(:lazy, fn state -> state end)
+      42
 
   """
-  def hello do
-    :world
+
+  defstruct started?: false,
+            start_fun: nil,
+            state: nil
+
+  @type t :: %__MODULE__{
+          started?: boolean,
+          start_fun: (() -> any),
+          state: any
+        }
+
+  @doc """
+  TODO
+  """
+  @spec start_link((() -> any), GenServer.options()) :: Agent.on_start()
+  def start_link(fun, options \\ []) do
+    if Application.get_env(:lazy_agent, :enabled?) do
+      Agent.start_link(
+        fn ->
+          %__MODULE__{started?: false, start_fun: fun}
+        end,
+        options
+      )
+    else
+      Agent.start_link(fun, options)
+    end
+  end
+
+  @doc """
+  TODO
+  """
+  @spec get(Agent.agent(), (any -> any)) :: any
+  def get(agent, fun) do
+    if Application.get_env(:lazy_agent, :enabled?) do
+      Agent.get_and_update(agent, fn lazy_state ->
+        new_lazy_state = prepare(lazy_state)
+        get_value = fun.(new_lazy_state.state)
+        {get_value, new_lazy_state}
+      end)
+    else
+      Agent.get(agent, fun)
+    end
+  end
+
+  @spec prepare(t()) :: t()
+  defp prepare(lazy_state = %__MODULE__{started?: true}), do: lazy_state
+
+  defp prepare(lazy_state = %__MODULE__{started?: false, start_fun: fun}) do
+    state = fun.()
+    %__MODULE__{lazy_state | started?: true, state: state}
   end
 end
--- test/lazy_agent_test.exs
+++ test/lazy_agent_test.exs
@@ -2,7 +2,44 @@ defmodule LazyAgentTest do
   use ExUnit.Case
   doctest LazyAgent
 
-  test "greets the world" do
-    assert LazyAgent.hello() == :world
+  describe "with lazy agent enabled" do
+    setup do
+      Application.put_env(:lazy_agent, :enabled, true)
+    end
+
+    test "start_link/2" do
+      start_fun = fn -> 45 end
+      {:ok, pid} = LazyAgent.start_link(start_fun)
+
+      assert Agent.get(pid, & &1) == %LazyAgent{started?: false, start_fun: start_fun, state: nil}
+    end
+
+    test "get/2" do
+      start_fun = fn -> 45 end
+      {:ok, pid} = LazyAgent.start_link(start_fun)
+
+      assert LazyAgent.get(pid, & &1) == 45
+      assert Agent.get(pid, & &1) == %LazyAgent{started?: true, start_fun: start_fun, state: 45}
+    end
+  end
+
+  describe "with LazyAgent disabled" do
+    setup do
+      Application.put_env(:lazy_agent, :enabled, false)
+    end
+
+    test "start_link/2" do
+      start_fun = fn -> 45 end
+      {:ok, pid} = LazyAgent.start_link(start_fun)
+
+      assert LazyAgent.get(pid, & &1) == 45
+    end
+
+    test "get/2" do
+      start_fun = fn -> 45 end
+      {:ok, pid} = LazyAgent.start_link(start_fun)
+
+      assert LazyAgent.get(pid, & &1) == 45
+    end
   end
 end
